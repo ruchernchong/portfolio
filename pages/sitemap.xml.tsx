7e88c671977aa358cf8e066de652ae9484696af4
Ru Chern Chong <hello@ruchern.xyz>
2022-12-24 00:38:59 +0800

    Update sitemap to include root domain

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,12 +1,11 @@
 import { GetServerSideProps } from "next";
-import { Post } from "lib/types";
 
 const generateSiteMap = (
-  posts: Post[]
+  slugs: string[]
 ) => `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     ${posts
-       .map(({ slug }) => {
+     ${slugs
+       .map((slug) => {
          return `
            <url>
                <loc>${`https://ruchern.xyz/${slug}`}</loc>
@@ -29,11 +28,12 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     },
   }).then((res) => res.json());
 
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
+  const pages = [...[""], ...posts.map(({ slug }) => `blog/${slug}`)];
+
+  // We generate the XML sitemap with the page slugs
+  const sitemap = generateSiteMap(pages);
 
   res.setHeader("Content-Type", "text/xml");
-  // we send the XML to the browser
   res.write(sitemap);
   res.end();
 
