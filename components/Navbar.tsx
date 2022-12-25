906847536fa3f301887f2b0dfb577454fb9042ab
Ru Chern Chong <hello@ruchern.xyz>
2022-12-26 04:41:32 +0800

    Refactor theme toggle into its own component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import NextLink from "next/link";
-import { useTheme } from "next-themes";
-
 import classNames from "classnames";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import ThemeToggle from "components/ThemeToggle";
 
 const NavItem = ({ href, title }) => {
   const router = useRouter();
@@ -25,12 +22,6 @@ const NavItem = ({ href, title }) => {
 };
 
 export default function Navbar() {
-  const [isMounted, setIsMounted] = useState<boolean>(false);
-  const { resolvedTheme, setTheme } = useTheme();
-  const isDarkMode = resolvedTheme === "dark";
-
-  useEffect(() => setIsMounted(true), []);
-
   return (
     <div className="sticky top-0 z-[1000] mx-auto mb-8 max-w-4xl bg-neutral-50 p-8 dark:bg-neutral-900">
       <nav className="flex items-center justify-between">
@@ -38,17 +29,7 @@ export default function Navbar() {
           <NavItem href="/" title="Home" />
           {/*<NavItem href="/projects" title="Projects" />*/}
         </div>
-        <button
-          type="button"
-          className="flex h-8 w-8 items-center justify-center rounded-lg bg-gray-200 ring-2 ring-neutral-600 transition-all hover:ring-2 dark:bg-neutral-900"
-          onClick={() => setTheme(isDarkMode ? "light" : "dark")}
-        >
-          {isMounted && (
-            <div className="h-6 w-6">
-              {isDarkMode ? <SunIcon /> : <MoonIcon />}
-            </div>
-          )}
-        </button>
+        <ThemeToggle />
       </nav>
     </div>
   );
---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+
+export default function ThemeToggle() {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const { resolvedTheme, setTheme } = useTheme();
+  const isDarkMode = resolvedTheme === "dark";
+
+  useEffect(() => setIsMounted(true), []);
+
+  return (
+    <button
+      type="button"
+      className="flex h-8 w-8 items-center justify-center rounded-lg bg-gray-200 ring-2 ring-neutral-600 transition-all hover:ring-2 dark:bg-neutral-900"
+      onClick={() => setTheme(isDarkMode ? "light" : "dark")}
+    >
+      {isMounted && (
+        <div className="h-6 w-6">{isDarkMode ? <SunIcon /> : <MoonIcon />}</div>
+      )}
+    </button>
+  );
+}
