a7677d7b030bbf099f5f7e2fb19eca26ca2d0b20
Ru Chern Chong <hello@ruchern.xyz>
2023-01-02 04:04:35 +0800

    Update font colours

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -7,11 +7,11 @@ const BlogPost = ({ title, slug, description, publishedAt }: Post) => (
     <div className="mb-8 transition hover:opacity-50">
       <h4 className="mb-2 w-full flex-col text-xl font-medium md:flex md:flex-row md:items-center md:justify-between">
         <span>{title}</span>
-        <p className="text-sm italic text-gray-500 dark:text-gray-400">
+        <p className="text-sm italic text-neutral-600 dark:text-neutral-400">
           {format(parseISO(publishedAt), "dd MMM yyyy")}
         </p>
       </h4>
-      <p className="text-gray-600 dark:text-gray-400">{description}</p>
+      <p className="text-neutral-600 dark:text-neutral-400">{description}</p>
     </div>
   </Link>
 );
