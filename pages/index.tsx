b2fa1c8ba6101d086b12d5378bff0641920d8471
Ru Chern Chong <hello@ruchern.xyz>
2023-01-02 04:04:23 +0800

    Add post count

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,12 @@ export default function Home({
           hideTagline={true}
         />
         <Suspense fallback={null}>
-          <h2 className="mb-6 text-3xl font-bold md:text-4xl">All Posts</h2>
+          <h2 className="mb-6 text-3xl font-bold md:text-4xl">
+            All Posts{" "}
+            <sup className="sups text-2xl text-neutral-600 dark:text-neutral-400">
+              ({posts.length})
+            </sup>
+          </h2>
           {posts.map(({ title, slug, description, published_at }) => {
             return (
               <BlogPost
