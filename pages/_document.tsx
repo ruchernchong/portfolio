abd23b28eae925bcf49fa7769d97974d01da6727
Ru Chern Chong <hello@ruchern.xyz>
2023-01-29 05:29:01 +0800

    Add RSS autodiscovery

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,21 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { HOST_URL } from "lib/config";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-50">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const Document = () => (
+  <Html lang="en">
+    <Head>
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="RSS feed for ruchern.xyz"
+        href={`${HOST_URL}/feed.xml`}
+      />
+    </Head>
+    <body className="bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-50">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
