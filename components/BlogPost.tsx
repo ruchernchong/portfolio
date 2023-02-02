2a32ac5ad8f10ef26700215dac6705a1014427ba
Ru Chern Chong <hello@ruchern.xyz>
2023-02-03 02:21:14 +0800

    Clean up blog entries on index page

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -6,21 +6,21 @@ const BlogPost = ({ title, slug, excerpt, publishedDate }: Post) => {
   const formattedDate = format(parseISO(publishedDate), "dd MMM yyyy");
 
   return (
-    <Link href={slug} className="w-full">
-      <div className="mb-8 transition hover:opacity-50">
-        <h3 className="mb-2 w-full flex-col text-xl font-medium md:flex md:flex-row md:items-center md:justify-between">
-          <span>{title}</span>
-          <time
-            dateTime={formatISO(parseISO(publishedDate))}
-            title={formattedDate}
-            className="block text-sm italic text-neutral-600 dark:text-neutral-400"
-          >
-            {formattedDate}
-          </time>
-        </h3>
-        <p className="text-neutral-600 dark:text-neutral-400">{excerpt}</p>
+    <div className="mb-8 w-full">
+      <div className="flex-col md:flex md:flex-row md:items-center md:justify-between">
+        <Link href={slug} className="transition hover:opacity-50">
+          <h3 className="text-xl font-medium">{title}</h3>
+        </Link>
+        <time
+          dateTime={formatISO(parseISO(publishedDate))}
+          title={formattedDate}
+          className="italic text-neutral-600 dark:text-neutral-400"
+        >
+          {formattedDate}
+        </time>
       </div>
-    </Link>
+      <p className="text-neutral-600 dark:text-neutral-400">{excerpt}</p>
+    </div>
   );
 };
 export default BlogPost;
