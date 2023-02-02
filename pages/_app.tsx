62842a80fe73f49d7fb257e9dfc584f029d1e100
Ru Chern Chong <hello@ruchern.xyz>
2023-02-03 01:38:12 +0800

    Add progress bar indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
 import "styles/globals.css";
+import "nprogress/nprogress.css";
 import { AnimatePresence } from "framer-motion";
+import NProgress from "nprogress";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteDone);
+    router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteDone);
+      router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
 
   return (
     <ThemeProvider attribute="class">
