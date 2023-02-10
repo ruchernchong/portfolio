f40c6548b287bf5abea506948a6c6ac43c0a53cf
Ru Chern Chong <hello@ruchern.xyz>
2023-02-10 23:16:43 +0800

    Update structured data

--- components/StructuredData.tsx
+++ components/StructuredData.tsx
@@ -14,7 +14,10 @@ interface Props {
 const StructuredData = ({ data }: Props) => {
   return (
     <Head>
-      <script type="application/ld+json">{JSON.stringify(data)}</script>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+      />
     </Head>
   );
 };
