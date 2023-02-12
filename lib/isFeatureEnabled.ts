2454ff78ae4571dac45d93a5cdf96e7eef6f43d6
Ru Chern Chong <hello@ruchern.xyz>
2023-02-13 01:38:14 +0800

    Add comment to isFeatureEnabled function

--- lib/isFeatureEnabled.ts
+++ lib/isFeatureEnabled.ts
@@ -1,2 +1,7 @@
+/**
+ * Check if a feature flag is enabled or disabled
+ *
+ * @param feature - Usually used with process.env
+ */
 export const isFeatureEnabled = (feature: string): boolean =>
   JSON.parse(feature);
