86921d9cf66db2f8ff63970c255397b38dde67e2
Ru Chern Chong <hello@ruchern.xyz>
2023-02-19 22:11:57 +0800

    Update MDX components

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,17 +1,35 @@
+import Link from "next/link";
 import Image from "next/image";
+
+const CustomLink = (props) => {
+  const href = props.href;
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
+
+  if (isInternalLink) {
+    return (
+      <Link href={href} {...props}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+};
+
 const ImageComponent = (props) => (
   <Image
     alt={props.alt}
     width={0}
     height={0}
     sizes="100vw"
-    className="rounded-lg w-auto h-auto"
+    className="h-auto w-auto rounded-2xl"
     {...props}
   />
 );
 
 const MDXComponents = {
-  img: ImageComponent
+  a: CustomLink,
+  img: ImageComponent,
 };
 
 export default MDXComponents;
