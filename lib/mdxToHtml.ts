f94d345a42cefb960fa1f4f19dd0428cc1b15bb1
Ru Chern Chong <hello@ruchern.xyz>
2023-02-26 12:33:26 +0800

    Add autolink headings

--- lib/mdxToHtml.ts
+++ lib/mdxToHtml.ts
@@ -3,11 +3,24 @@ import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 import rehypeCodeTitles from "rehype-code-titles";
 import rehypePrism from "rehype-prism-plus";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export const mdxToHtml = async (content: string) =>
   serialize(content, {
     mdxOptions: {
       remarkPlugins: [remarkGfm],
-      rehypePlugins: [rehypeSlug, rehypeCodeTitles, rehypePrism]
-    }
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeCodeTitles,
+        rehypePrism,
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: {
+              className: ["anchor"],
+            },
+          },
+        ],
+      ],
+    },
   });
