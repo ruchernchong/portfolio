f77121c0a86f146bcd0f425193df7fd95d57979f
Ru Chern Chong <hello@ruchern.xyz>
2023-03-04 04:50:53 +0800

    Refactor Contributions into its own component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import Layout from "components/Layout";
 import Author from "components/Author";
+import Contributions from "components/Contributions";
 import Employment from "components/Employment";
 import StructuredData from "components/StructuredData";
 import companies from "data/companies";
@@ -34,19 +35,7 @@ const About = () => {
       </div>
       <Employment companies={sortedCompanies} />
       {isFeatureEnabled(process.env.NEXT_PUBLIC_FEATURE_CONTRIBUTIONS) && (
-        <section className="prose prose-neutral mx-auto mb-8 max-w-4xl dark:prose-invert">
-          <h2 className="text-2xl font-bold md:text-3xl">Contributions</h2>
-          <div className="mb-8 space-y-4">
-            <div className="flex items-center space-x-4">
-              <h3 className="m-0 text-lg font-semibold md:text-2xl">GitHub</h3>
-            </div>
-            <div className="flex items-center space-x-4">
-              <h3 className="m-0 text-lg font-semibold md:text-2xl">
-                Stack Overflow
-              </h3>
-            </div>
-          </div>
-        </section>
+        <Contributions />
       )}
     </Layout>
   );
