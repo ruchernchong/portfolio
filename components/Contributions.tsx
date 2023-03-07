a2c7011ad3be330acd634a39a609da31820d7b48
Ru Chern Chong <hello@ruchern.xyz>
2023-03-07 21:17:29 +0800

    Move GitHub pinned repositories to Project page

--- components/Contributions.tsx
+++ components/Contributions.tsx
@@ -20,42 +20,42 @@ const Contributions = ({
   return (
     <section className="prose prose-neutral mx-auto mb-8 max-w-4xl dark:prose-invert">
       <h2 className="text-2xl md:text-3xl">Contributions</h2>
-      <div className="mb-8 flex flex-col">
-        <h3 className="m-0 text-lg font-semibold md:text-2xl">GitHub</h3>
-        <p className="text-sm italic text-neutral-600 dark:text-neutral-400">
-          (Powered by GitHub GraphQL API)
-        </p>
-        <h4 className="m-0 mb-4 text-lg">
-          <div className="flex flex-row items-center">
-            <BookmarkIcon className="mr-2 h-6 w-6" />{" "}
-            <span>Pinned repositories</span>
-          </div>
-        </h4>
-        <div className="grid gap-4 md:grid-cols-2">
-          {pinnedRepositories.map(
-            ({ id, name, description, stargazers, url }) => {
-              return (
-                <a
-                  key={id}
-                  href={url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex flex-col rounded-2xl border p-4 no-underline md:border-neutral-600 hover:md:border-neutral-400"
-                >
-                  <div className="text-xl">{name}</div>
-                  <div className="flex-1 text-neutral-600 dark:text-neutral-400">
-                    {description}
-                  </div>
-                  <div className="flex flex-row items-center">
-                    <StarIcon className="mr-2 h-4 w-4" />
-                    <div>{stargazers.totalCount}</div>
-                  </div>
-                </a>
-              );
-            }
-          )}
-        </div>
-      </div>
+      {/*<div className="mb-8 flex flex-col">*/}
+      {/*  <h3 className="m-0 text-lg font-semibold md:text-2xl">GitHub</h3>*/}
+      {/*  <p className="text-sm italic text-neutral-600 dark:text-neutral-400">*/}
+      {/*    (Powered by GitHub GraphQL API)*/}
+      {/*  </p>*/}
+      {/*  <h4 className="m-0 mb-4 text-lg">*/}
+      {/*    <div className="flex flex-row items-center">*/}
+      {/*      <BookmarkIcon className="mr-2 h-6 w-6" />{" "}*/}
+      {/*      <span>Pinned repositories</span>*/}
+      {/*    </div>*/}
+      {/*  </h4>*/}
+      {/*  <div className="grid gap-4 md:grid-cols-2">*/}
+      {/*    {pinnedRepositories.map(*/}
+      {/*      ({ id, name, description, stargazers, url }) => {*/}
+      {/*        return (*/}
+      {/*          <a*/}
+      {/*            key={id}*/}
+      {/*            href={url}*/}
+      {/*            target="_blank"*/}
+      {/*            rel="noopener noreferrer"*/}
+      {/*            className="flex flex-col rounded-2xl border p-4 no-underline md:border-neutral-600 hover:md:border-neutral-400"*/}
+      {/*          >*/}
+      {/*            <div className="text-xl">{name}</div>*/}
+      {/*            <div className="flex-1 text-neutral-600 dark:text-neutral-400">*/}
+      {/*              {description}*/}
+      {/*            </div>*/}
+      {/*            <div className="flex flex-row items-center">*/}
+      {/*              <StarIcon className="mr-2 h-4 w-4" />*/}
+      {/*              <div>{stargazers.totalCount}</div>*/}
+      {/*            </div>*/}
+      {/*          </a>*/}
+      {/*        );*/}
+      {/*      }*/}
+      {/*    )}*/}
+      {/*  </div>*/}
+      {/*</div>*/}
       <div className="mb-8 flex flex-col text-neutral-600 dark:text-neutral-400">
         <h3 className="m-0 text-lg font-semibold md:text-2xl">
           Stack Overflow
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,59 @@
+import { GetStaticProps } from "next";
 import Layout from "@/components/Layout";
+import { BookmarkIcon, StarIcon } from "@heroicons/react/24/outline";
+import { getGitHubPinnedRepositories } from "@/lib/github";
 
-const Projects = () => {
+const Projects = ({ pinnedRepositories }) => {
   return (
     <Layout title="Projects - Ru Chern">
-      <h1 className="mb-8 text-center text-4xl">Coming Soon!</h1>
+      <div className="mb-8 flex flex-col">
+        <h3 className="m-0 text-lg font-semibold md:text-2xl">GitHub</h3>
+        <p className="text-sm italic text-neutral-600 dark:text-neutral-400">
+          (Powered by GitHub GraphQL API)
+        </p>
+        <h4 className="m-0 mb-4 text-lg">
+          <div className="flex flex-row items-center">
+            <BookmarkIcon className="mr-2 h-6 w-6" />{" "}
+            <span>Pinned repositories</span>
+          </div>
+        </h4>
+        <div className="grid gap-4 md:grid-cols-2">
+          {pinnedRepositories.map(
+            ({ id, name, description, stargazers, url }) => {
+              return (
+                <a
+                  key={id}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex flex-col rounded-2xl border p-4 no-underline md:border-neutral-600 hover:md:border-neutral-400"
+                >
+                  <div className="text-xl">{name}</div>
+                  <div className="flex-1 text-neutral-600 dark:text-neutral-400">
+                    {description}
+                  </div>
+                  <div className="flex flex-row items-center">
+                    <StarIcon className="mr-2 h-4 w-4" />
+                    <div>{stargazers.totalCount}</div>
+                  </div>
+                </a>
+              );
+            }
+          )}
+        </div>
+      </div>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const pinnedRepositories = await getGitHubPinnedRepositories();
+
+  return {
+    props: {
+      pinnedRepositories,
+    },
+  };
 };
 
 export default Projects;
