1f4654f3c08380b527c2531ed60314f27aa55920
Ru Chern Chong <hello@ruchern.xyz>
2023-03-12 01:50:13 +0800

    Remove backtick on code tag from Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,29 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+            code: {
+              color: theme("colors.neutral.500"),
+              backgroundColor: theme("colors.neutral.200"),
+              borderRadius: theme("borderRadius.DEFAULT"),
+              paddingLeft: theme("spacing[1.5]"),
+              paddingRight: theme("spacing[1.5]"),
+              paddingTop: theme("spacing.1"),
+              paddingBottom: theme("spacing.1"),
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
