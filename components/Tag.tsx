e5d2e9ef8d1a29bc557f3cda9b6c6b76ae833fe1
Ru Chern Chong <hello@ruchern.xyz>
2023-03-28 01:41:25 +0800

    Update Tag component styling

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,15 +1,26 @@
+import { PropsWithChildren } from "react";
 import classNames from "classnames";
 
-const Tag = (props) => {
-  const isSelected = props.isSelected;
+type Variant = "outlined" | "filled";
+type Size = "small" | "medium";
 
+interface TagProps extends PropsWithChildren {
+  variant?: Variant;
+  size?: Size;
+}
+
+const Tag = ({ variant = "outlined", size = "medium", ...props }: TagProps) => {
   return (
     <button
       type="button"
       className={classNames(
-        "rounded-full border border-blue-600 bg-white px-4 py-2 pl-8 text-center text-base font-medium text-blue-700 before:absolute before:-ml-4 before:content-['#'] hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:bg-neutral-900 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800",
+        "rounded-full bg-white px-4 py-2 pl-8 text-center text-base font-medium text-blue-700 before:absolute before:-ml-4 before:content-['#'] hover:bg-blue-700 hover:text-white dark:border-blue-500 dark:bg-neutral-900 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white",
         {
-          "bg-blue-700 text-white dark:bg-blue-500 dark:text-white": isSelected,
+          "py-1": size === "small",
+          "bg-blue-700 text-white dark:bg-blue-500 dark:text-white":
+            variant === "filled",
+          "border border-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800":
+            variant === "outlined",
         }
       )}
       {...props}
