eb563666ce0173a8ad2aa62c76c76284fb0c6eb2
Ru Chern Chong <hello@ruchern.xyz>
2023-05-13 23:07:39 +0800

    Update to use Turbopack

--- next.config.js
+++ next.config.js
@@ -7,15 +7,6 @@ const nextConfig = {
   async headers() {
     return [{ source: "/:path*", headers: securityHeaders }];
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
 
 // Recommended configuration: https://nextjs.org/docs/advanced-features/security-headers
