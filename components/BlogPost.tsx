5bd61573dd9a746536e400a67d405f02886dfd8c
Ru Chern Chong <hello@ruchern.xyz>
2023-05-13 13:28:38 +0800

    Add read more to each blog post on index page

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,27 +1,33 @@
 import Link from "next/link";
 import { format, formatISO, parseISO } from "date-fns";
-import { ArrowSmallRightIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import { Post } from "@/lib/types";
 
 const BlogPost = ({ title, slug, excerpt, publishedDate }: Post) => {
   const formattedDate = format(parseISO(publishedDate), "iiii, dd MMMM yyyy");
 
   return (
-    <div className="group relative flex flex-col-reverse md:flex-row md:justify-between">
-      <div className="w-full basis-2/3">
-        <Link href={slug}>
-          <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 rounded-2xl border border-neutral-600 opacity-0 transition group-hover:scale-100 group-hover:bg-neutral-800/25 group-hover:opacity-100" />
-          <h3 className="text-2xl font-medium md:mb-2">{title}</h3>
-        </Link>
-        <p className="text-neutral-600 dark:text-neutral-400">{excerpt}</p>
+    <div className="group relative">
+      <div className="mb-4 flex flex-col-reverse md:flex-row md:justify-between">
+        <div className="w-full basis-2/3">
+          <Link href={slug}>
+            <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 rounded-2xl border border-neutral-600 opacity-0 transition group-hover:scale-100 group-hover:bg-neutral-800/25 group-hover:opacity-100" />
+            <h3 className="text-2xl font-medium">{title}</h3>
+          </Link>
+          <p className="text-neutral-600 dark:text-neutral-400">{excerpt}</p>
+        </div>
+        <time
+          dateTime={formatISO(parseISO(publishedDate))}
+          title={formattedDate}
+          className="italic text-neutral-600 dark:text-neutral-400"
+        >
+          {formattedDate}
+        </time>
       </div>
-      <time
-        dateTime={formatISO(parseISO(publishedDate))}
-        title={formattedDate}
-        className="italic text-neutral-600 dark:text-neutral-400"
-      >
-        {formattedDate}
-      </time>
+      <div className="flex items-center">
+        <div className="text-purple-300">Read more</div>
+        <ChevronRightIcon width={16} height={16} />
+      </div>
     </div>
   );
 };
