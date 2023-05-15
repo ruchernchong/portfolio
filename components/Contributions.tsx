e087a859decdffb15dde6efb5523097046957693
Ru Chern Chong <hello@ruchern.xyz>
2023-05-16 02:27:53 +0800

    Fix error rendering when API fails to fetch data

--- components/Contributions.tsx
+++ components/Contributions.tsx
@@ -20,46 +20,50 @@ const Contributions = ({
   return (
     <section className="prose prose-neutral mx-auto max-w-4xl dark:prose-invert">
       <h2 className="text-2xl md:text-3xl">Contributions</h2>
-      <div className="mb-8 flex flex-col">
-        <h3 className="m-0 text-lg font-semibold md:text-2xl">GitHub</h3>
-        <p className="mb-2 text-sm italic text-neutral-600 dark:text-neutral-400">
-          (Powered by GitHub GraphQL API)
-        </p>
-        <div>
-          Total commits:{" "}
-          {github.contributionsCollection.totalCommitContributions}
+      {github && (
+        <div className="mb-8 flex flex-col">
+          <h3 className="m-0 text-lg font-semibold md:text-2xl">GitHub</h3>
+          <p className="mb-2 text-sm italic text-neutral-600 dark:text-neutral-400">
+            (Powered by GitHub GraphQL API)
+          </p>
+          <div>
+            Total commits:{" "}
+            {github.contributionsCollection.totalCommitContributions}
+          </div>
+          <div>Pull requests: {github.pullRequests.totalCount}</div>
+          <div className="mb-2">Followers: {github.followers.totalCount}</div>
+          <div className="flex flex-col items-start">
+            <LinkWithIcon url={github.url} />
+          </div>
         </div>
-        <div>Pull requests: {github.pullRequests.totalCount}</div>
-        <div className="mb-2">Followers: {github.followers.totalCount}</div>
-        <div className="flex flex-col items-start">
-          <LinkWithIcon url={github.url} />
+      )}
+      {stackOverflow && (
+        <div className="flex flex-col text-neutral-600 dark:text-neutral-400">
+          <h3 className="m-0 text-lg font-semibold md:text-2xl">
+            Stack Overflow
+          </h3>
+          <p className="text-sm italic text-neutral-600 dark:text-neutral-400">
+            (Powered by Stack Exchange API)
+          </p>
+          <div>Reputation: {stackOverflow.reputation}</div>
+          <div className="flex items-center gap-2">
+            {Object.entries(stackOverflow.badge_counts)
+              .reverse()
+              .map(([tier, count]) => {
+                return (
+                  <Fragment key={tier}>
+                    <span
+                      className={classNames(
+                        `h-4 w-4 rounded-full ${STACK_OVERFLOW_BADGES[tier]}`
+                      )}
+                    />
+                    <div>{count}</div>
+                  </Fragment>
+                );
+              })}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col text-neutral-600 dark:text-neutral-400">
-        <h3 className="m-0 text-lg font-semibold md:text-2xl">
-          Stack Overflow
-        </h3>
-        <p className="text-sm italic text-neutral-600 dark:text-neutral-400">
-          (Powered by Stack Exchange API)
-        </p>
-        <div>Reputation: {stackOverflow.reputation}</div>
-        <div className="flex items-center gap-2">
-          {Object.entries(stackOverflow.badge_counts)
-            .reverse()
-            .map(([tier, count]) => {
-              return (
-                <Fragment key={tier}>
-                  <span
-                    className={classNames(
-                      `h-4 w-4 rounded-full ${STACK_OVERFLOW_BADGES[tier]}`
-                    )}
-                  />
-                  <div>{count}</div>
-                </Fragment>
-              );
-            })}
-        </div>
-      </div>
+      )}
     </section>
   );
 };
