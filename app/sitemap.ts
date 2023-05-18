7caa61fae8c4a845b6a609735c4d382b73e1e728
Ru Chern Chong <hello@ruchern.xyz>
2023-05-18 16:00:42 +0800

    Update index page in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,15 +5,19 @@ import { sanityClient } from "@/lib/sanity-server";
 import { postsQuery } from "@/lib/queries";
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
-  const pages = ["", "about", "random-musings", "projects"];
+  const pages = ["/about", "/random-musings", "/projects"];
   const posts: Post[] = await sanityClient.fetch(postsQuery);
   const randomMusings: RandomMusing[] = await fetch(
     "https://raw.githubusercontent.com/ruchernchong/random-musings/main/feed.json"
   ).then((res) => res.json());
 
   return [
+    {
+      url: HOST_URL,
+      lastModified: formatLastModified(),
+    },
     ...pages.map((page) => ({
-      url: `${HOST_URL}/${page}`,
+      url: `${HOST_URL}${page}`,
       lastModified: formatLastModified(),
     })),
     ...posts.map(({ publishedDate, slug }) => ({
