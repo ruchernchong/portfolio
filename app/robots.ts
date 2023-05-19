3a299053c94824fc9179b7f40324af012eb37b4a
Ru Chern Chong <hello@ruchern.xyz>
2023-05-20 02:41:10 +0800

    Update robots.txt for sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ const robots = (): MetadataRoute.Robots => {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${HOST_URL}/sitemap.xml`,
+    sitemap: `${HOST_URL}/sitemap.xml/`,
   };
 };
 
