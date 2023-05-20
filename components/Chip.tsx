f074f6546320a159c730d834233a53c62e746e91
Ru Chern Chong <hello@ruchern.xyz>
2023-05-20 12:32:23 +0800

    Update accent colour of Chip component

--- components/Chip.tsx
+++ components/Chip.tsx
@@ -18,12 +18,12 @@ const Chip = ({
     <button
       type="button"
       className={classNames(
-        "rounded-full bg-white px-4 py-2 pl-8 text-center text-base font-medium text-blue-700 before:absolute before:-ml-4 before:content-['#'] hover:bg-blue-700 hover:text-white dark:border-blue-500 dark:bg-neutral-900 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white",
+        "rounded-full bg-white px-4 py-2 pl-8 text-center text-base font-medium text-blue-700 before:absolute before:-ml-4 before:content-['#'] hover:bg-blue-700 hover:text-white dark:border-indigo-300 dark:bg-neutral-900 dark:text-indigo-300 dark:hover:bg-indigo-300 dark:hover:text-neutral-900",
         {
           "py-1": size === "small",
-          "bg-blue-700 text-white dark:bg-blue-500 dark:text-white":
+          "bg-blue-700 text-white dark:bg-indigo-300 dark:text-neutral-900":
             variant === "filled",
-          "border border-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800":
+          "border border-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-indigo-800":
             variant === "outlined",
         }
       )}
