17106346d1e24376c7251a66d76003bbc54e188d
Ru Chern Chong <hello@ruchern.xyz>
2023-08-21 01:22:54 +0800

    Fix alignment in Author component

--- components/Author.tsx
+++ components/Author.tsx
@@ -12,7 +12,7 @@ type Author = {
 
 const Author = ({ title, tagline, description }: Author) => {
   return (
-    <div className="flex flex-col-reverse items-center md:flex-row md:items-center md:gap-8">
+    <div className="flex flex-col-reverse items-center md:flex-row md:items-start md:gap-8">
       <div className="flex grow basis-1/2 flex-col gap-4">
         <div className="flex flex-col items-center md:items-start">
           <H1>{title}</H1>
