8bdb645df37ca30f38d4139f52e3927efa7ae087
Ru Chern Chong <hello@ruchern.xyz>
2023-09-18 03:10:29 +0800

    Update sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,12 @@
 import { MetadataRoute } from "next";
 import { HOST_URL } from "@/config";
-import { Post, RandomMusing } from "@/lib/types";
 import { sanityClient } from "@/lib/sanity-server";
 import { postsQuery } from "@/lib/queries";
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   const pages = ["/about", "/random-musings", "/projects"];
-  const posts: Post[] = await sanityClient.fetch(postsQuery);
-  const randomMusings: RandomMusing[] = await fetch(
+  const posts = await sanityClient.fetch(postsQuery);
+  const randomMusings = await fetch(
     "https://raw.githubusercontent.com/ruchernchong/random-musings/main/feed.json"
   ).then((res) => res.json());
 
@@ -15,18 +14,26 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
     {
       url: HOST_URL,
       lastModified: formatLastModified(),
+      changeFrequency: "monthly",
+      priority: 1,
     },
     ...pages.map((page) => ({
       url: `${HOST_URL}${page}`,
       lastModified: formatLastModified(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     })),
     ...posts.map(({ publishedDate, slug }) => ({
       url: `${HOST_URL}/blog/${slug}`,
       lastModified: formatLastModified(publishedDate),
+      changeFrequency: "daily",
+      priority: 0.5,
     })),
     ...randomMusings.map(({ date, slug }) => ({
       url: `${HOST_URL}/random-musings/${slug}`,
       lastModified: formatLastModified(date),
+      changeFrequency: "daily",
+      priority: 0.5,
     })),
   ];
 };
