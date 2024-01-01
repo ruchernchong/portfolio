3a712bfb5594ac7f4b79d46a04cda843a7011782
Ru Chern Chong <hello@ruchern.xyz>
2024-01-02 02:16:18 +0800

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,34 @@
-import { MetadataRoute } from "next";
 import { allDocuments } from "contentlayer/generated";
 import { BASE_URL, navLinks } from "@/config";
 
-const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+const sitemap = async () => {
   const pages = navLinks
     .filter(({ href }) => href !== "/")
     .map(({ href }) => href);
 
   return [
-    { url: BASE_URL, lastModified: formatLastModified() },
+    {
+      url: BASE_URL,
+      lastModified: formatLastModified(),
+      changeFrequency: "yearly",
+      priority: 1.0,
+    },
     ...pages.map((url) => ({
       url: `${BASE_URL}${url}`,
       lastModified: formatLastModified(),
+      changeFrequency: "yearly",
+      priority: 1.0,
     })),
     ...allDocuments.map(({ publishedAt, url }) => ({
       url: `${BASE_URL}${url}`,
       lastModified: formatLastModified(publishedAt),
+      changeFrequency: "weekly",
+      priority: 0.7,
     })),
   ];
 };
 
-const formatLastModified = (datetime?: string | Date) => {
+const formatLastModified = (datetime?: Date | string) => {
   if (typeof datetime === "string") {
     datetime = new Date(datetime).toISOString().split("T")[0];
   }
