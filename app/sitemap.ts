51fba456ff969857c6b033b50b233b8ce2d44cde
Ru Chern Chong <hello@ruchern.xyz>
2024-01-28 23:09:07 +0800

    Clean up sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,26 +2,22 @@ import { MetadataRoute } from "next";
 import { allDocuments } from "contentlayer/generated";
 import { BASE_URL, navLinks } from "@/config";
 
-const sitemap = (): MetadataRoute.Sitemap => {
-  const pages = navLinks
+const sitemap = (): MetadataRoute.Sitemap => [
+  {
+    url: BASE_URL,
+    lastModified: formatLastModified(),
+  },
+  ...navLinks
     .filter(({ href }) => href !== "/")
-    .map(({ href }) => href);
-
-  return [
-    {
-      url: BASE_URL,
-      lastModified: formatLastModified(),
-    },
-    ...pages.map((url) => ({
-      url: `${BASE_URL}${url}`,
+    .map(({ href }) => ({
+      url: `${BASE_URL}${href}`,
       lastModified: formatLastModified(),
     })),
-    ...allDocuments.map(({ publishedAt, url }) => ({
-      url: `${BASE_URL}${url}`,
-      lastModified: formatLastModified(publishedAt),
-    })),
-  ];
-};
+  ...allDocuments.map(({ publishedAt, url }) => ({
+    url: `${BASE_URL}${url}`,
+    lastModified: formatLastModified(publishedAt),
+  })),
+];
 
 const formatLastModified = (datetime: Date | string = new Date()): string =>
   new Date(datetime).toISOString().split("T")[0];
