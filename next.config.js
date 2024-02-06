3bfccadcee6b9731bdd4cbde68a2c9661f9b68d4
Ru Chern Chong <hello@ruchern.xyz>
2024-02-06 15:45:04 +0800

    Enable logging of full URL in dev

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
