16ff98df1d6f31a22236c598ba0b857aa2e2d031
Ru Chern Chong <hello@ruchern.xyz>
2024-02-06 10:18:53 +0800

    Remove SST

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { Config } from "sst/node/config";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    datasourceUrl: Config.DATABASE_URL,
+    datasourceUrl: process.env.DATABASE_URL,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
