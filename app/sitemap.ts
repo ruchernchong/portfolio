04330f51931144e1be96843393b28433643877c7
Ru Chern Chong <hello@ruchern.xyz>
2024-02-10 00:18:13 +0800

    Set sitemap to not cache
    Was returning HTTP 304

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,28 @@
 import type { MetadataRoute } from "next";
+import { unstable_noStore as noStore } from "next/cache";
 import { allDocuments } from "contentlayer/generated";
 import { BASE_URL, navLinks } from "@/config";
 
-const sitemap = (): MetadataRoute.Sitemap => [
-  {
-    url: BASE_URL,
-    lastModified: formatLastModified(),
-  },
-  ...navLinks
-    .filter(({ href }) => href !== "/")
-    .map(({ href }) => ({
-      url: `${BASE_URL}${href}`,
+const sitemap = (): MetadataRoute.Sitemap => {
+  noStore();
+
+  return [
+    {
+      url: BASE_URL,
       lastModified: formatLastModified(),
+    },
+    ...navLinks
+      .filter(({ href }) => href !== "/")
+      .map(({ href }) => ({
+        url: `${BASE_URL}${href}`,
+        lastModified: formatLastModified(),
+      })),
+    ...allDocuments.map(({ publishedAt, url }) => ({
+      url: `${BASE_URL}${url}`,
+      lastModified: formatLastModified(publishedAt),
     })),
-  ...allDocuments.map(({ publishedAt, url }) => ({
-    url: `${BASE_URL}${url}`,
-    lastModified: formatLastModified(publishedAt),
-  })),
-];
+  ];
+};
 
 const formatLastModified = (datetime: Date | string = new Date()): string =>
   new Date(datetime).toISOString().split("T")[0];
