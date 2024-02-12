70cdb68ae35d430ed86fdb8dda6d0ed5093c746b
Ru Chern Chong <hello@ruchern.xyz>
2024-02-12 18:25:54 +0800

    Remove Likes model

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,16 +1,8 @@
 -- CreateTable
-CREATE TABLE `Views` (
-    `slug` VARCHAR(191) NOT NULL,
-    `count` INTEGER NOT NULL DEFAULT 1,
+CREATE TABLE "Views" (
+    "slug" TEXT NOT NULL,
+    "count" INTEGER NOT NULL DEFAULT 1,
 
-    PRIMARY KEY (`slug`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Likes` (
-    `slug` VARCHAR(191) NOT NULL,
-    `count` INTEGER NOT NULL DEFAULT 1,
-
-    PRIMARY KEY (`slug`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "Views_pkey" PRIMARY KEY ("slug")
+);
 
