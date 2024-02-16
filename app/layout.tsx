e4d8221cf18145eaf5eac3ce524b4e0e914e7e84
Ru Chern Chong <hello@ruchern.xyz>
2024-02-16 12:11:41 +0800

    Remove suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+    <html lang="en" className="scroll-smooth">
       <body className="bg-gray-900 text-gray-50">
         <div className="flex min-h-screen flex-col">
           <Header />
