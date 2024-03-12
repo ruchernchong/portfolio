a77f9b27f75e6edc772da957ac5fc4a1f8a46422
Ru Chern Chong <hello@ruchern.xyz>
2024-03-13 05:00:51 +0800

    Add animation to gradient text

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,21 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        animatedGradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedGradient 6s ease infinite alternate",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
