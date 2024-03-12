43ae3b0ae4caf247052aa7472845ce2319b7f8ce
Ru Chern Chong <hello@ruchern.xyz>
2024-03-13 01:42:48 +0800

    Add resume data

---
+++ src/data/resume.ts
@@ -0,0 +1,69 @@
+import { DOMAIN_NAME } from "../config";
+
+export default {
+  baseUrl: `https://resume.${DOMAIN_NAME}`,
+  name: "Chong Ru Chern",
+  jobDescription: "Web Developer",
+  about: ``,
+  experience: [
+    {
+      name: "DBS Bank",
+      location: "Singapore",
+      startDate: "Apr 2021",
+      endDate: "Present",
+      description: [],
+    },
+    {
+      name: "Avanade",
+      location: "Singapore",
+      startDate: "Jan 2021",
+      endDate: "Apr 2021",
+      description: [],
+    },
+    {
+      name: "Singtel",
+      location: "Singapore",
+      startDate: "Aug 2018",
+      endDate: "Dec 2020",
+      description: [],
+    },
+    {
+      name: "Sproud",
+      location: "Singapore",
+      startDate: "Feb 2017",
+      endDate: "Aug 2018",
+      description: [],
+    },
+    {
+      name: "The University of Queensland",
+      location: "Brisbane, Queensland, Australia",
+      startDate: "Feb 2016",
+      endDate: "Jun 2016",
+      description: [],
+    },
+  ],
+  education: [
+    {
+      name: "The University of Queensland",
+      location: "Brisbane, Queensland, Australia",
+      startDate: "Feb 2015",
+      endDate: "Aug 2016",
+      description: ["Bachelor"],
+      course: "Information Technology",
+    },
+    {
+      name: "Singapore Polytechnic",
+      location: "Singapore",
+      startDate: "Apr 2009",
+      endDate: "May 2012",
+      description: ["Diploma"],
+      course: "Information Communication Technology",
+    },
+  ],
+  socialMedia: {
+    github: "https://github.com/ruchernchong",
+    linkedin: "https://www.linkedin.com/in/ruchernchong/",
+    twitter: "https://twitter.com/ruchernchong",
+  },
+  projects: [],
+};
