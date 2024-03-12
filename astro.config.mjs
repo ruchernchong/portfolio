aa33dd6162fb84148f1dc3e645a8f3498467fec1
Ru Chern Chong <hello@ruchern.xyz>
2024-03-13 04:37:15 +0800

    Add Sitemap and Robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import { DOMAIN_NAME } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: `https://resume.${DOMAIN_NAME}`,
+  integrations: [tailwind(), icon(), sitemap()],
 });
