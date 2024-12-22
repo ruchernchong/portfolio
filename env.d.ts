099691a869dc5697c1856c6b11330922034998db
Ru Chern Chong <hello@ruchern.dev>
2024-12-23 00:35:41 +0800

    Add Media collection

--- env.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    NEXT_PUBLIC_BASE_URL: string;
-    DATABASE_URL: string;
-  }
-}
--- payload.config.ts
+++ payload.config.ts
@@ -3,10 +3,29 @@ import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
 import sharp from "sharp";
 import Posts from "./collections/Posts";
+import { s3Storage } from "@payloadcms/storage-s3";
+import Media from "./collections/Media";
 
 export default buildConfig({
   editor: lexicalEditor(),
-  collections: [Posts],
+  collections: [Media, Posts],
+  plugins: [
+    s3Storage({
+      collections: {
+        media: {
+          prefix: "media",
+        },
+      },
+      bucket: process.env.S3_BUCKET!,
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+        },
+        region: process.env.S3_REGION,
+      },
+    }),
+  ],
   secret: process.env.PAYLOAD_SECRET || "",
   db: postgresAdapter({
     pool: {
