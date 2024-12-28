79228466b6947674a19178d2464d80e7ae918cf8
Ru Chern Chong <hello@ruchern.dev>
2024-12-28 21:43:30 +0800

    Add Vercel adapter

--- apps/resume/astro.config.mjs
+++ apps/resume/astro.config.mjs
@@ -4,8 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { DOMAIN_NAME } from "./src/config";
 
-// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   site: `https://resume.${DOMAIN_NAME}`,
   integrations: [tailwind(), icon(), sitemap()],
+  output: "server",
+  adapter: vercel(),
 });
