c17024abb6ff00446717096207777bca4f6d1173
Ru Chern Chong <hello@ruchern.dev>
2025-01-12 02:22:51 +0800

    Update stats bar layout and responsiveness

--- apps/blog/components/StatsBar.tsx
+++ apps/blog/components/StatsBar.tsx
@@ -8,17 +8,12 @@ interface StatsBarProps {
 
 const StatsBar = ({ slug }: StatsBarProps) => {
   return (
-    <div className="md:fixed md:inset-0 md:flex md:items-center md:justify-end">
-      <div className="hidden flex-col items-center gap-4 rounded-lg bg-gray-800/50 p-4 backdrop-blur md:flex">
+    <div className="fixed bottom-0 left-0 right-0 md:inset-0 md:flex md:items-center md:justify-end">
+      <div
+        className={`flex w-full items-center justify-center gap-4 bg-gray-800/50 p-2 backdrop-blur md:w-auto md:flex-col md:items-center md:rounded-lg md:p-4`}
+      >
         <LikeCounter slug={slug} />
-        <div className="flex flex-col items-center gap-2">
-          <EyeIcon className="h-6 w-6 text-neutral-400" />
-          <ViewCounter slug={slug} />
-        </div>
-      </div>
-      <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-center gap-4 bg-gray-800/50 p-2 backdrop-blur md:hidden">
-        <LikeCounter slug={slug} />
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 md:flex-col">
           <EyeIcon className="h-6 w-6 text-neutral-400" />
           <ViewCounter slug={slug} />
         </div>
