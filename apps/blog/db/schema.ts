53270b4ad3ce3c69b0cda83bb2f73f3c725a514d
Ru Chern Chong <hello@ruchern.dev>
2025-01-14 02:34:00 +0800

    Add indexing

--- apps/blog/db/schema.ts
+++ apps/blog/db/schema.ts
@@ -1,5 +1,6 @@
 import {
   decimal,
+  index,
   integer,
   pgTable,
   text,
@@ -7,25 +8,34 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
-export const sessions = pgTable("sessions", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  createdAt: timestamp("created_at", { withTimezone: true })
-    .defaultNow()
-    .notNull(),
-  path: text("path").notNull(),
-  referrer: text("referrer"),
-  city: text("city"),
-  country: text("country"),
-  flag: text("flag"),
-  latitude: decimal("latitude", { precision: 8, scale: 6 }),
-  longitude: decimal("longitude", { precision: 9, scale: 6 }),
-  browser: text("browser"),
-  os: text("os"),
-  device: text("device"),
-  screen: text("screen"),
-  language: text("language"),
-  duration: integer("duration"),
-});
+export const sessions = pgTable(
+  "sessions",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+    path: text("path").notNull(),
+    referrer: text("referrer"),
+    city: text("city"),
+    country: text("country"),
+    flag: text("flag"),
+    latitude: decimal("latitude", { precision: 8, scale: 6 }),
+    longitude: decimal("longitude", { precision: 9, scale: 6 }),
+    browser: text("browser"),
+    os: text("os"),
+    device: text("device"),
+    screen: text("screen"),
+    language: text("language"),
+    duration: integer("duration"),
+  },
+  (table) => ({
+    createdAtIdx: index("created_at_idx").on(table.createdAt),
+    pathIdx: index("path_idx").on(table.path),
+    countryIdx: index("country_idx").on(table.country),
+    browserIdx: index("browser_idx").on(table.browser),
+  }),
+);
 
 export type Session = typeof sessions.$inferSelect;
 export type NewSession = typeof sessions.$inferInsert;
