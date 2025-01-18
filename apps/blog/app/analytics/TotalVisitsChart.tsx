fe1b40750d3a1334bde269b37c65c923c358b58e
Ru Chern Chong <hello@ruchern.dev>
2025-01-19 00:00:13 +0800

    Add time duration in chart

--- apps/blog/app/analytics/TotalVisitsChart.tsx
+++ apps/blog/app/analytics/TotalVisitsChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
-import React from "react";
+import React, { useState } from "react";
 import {
   type ChartConfig,
   ChartContainer,
@@ -10,8 +10,21 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import type { Visit } from "../actions/analytics/visits";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface Props {
   data: Visit[];
@@ -31,14 +44,54 @@ const formatDate = (date: string) =>
   });
 
 const TotalVisitsChart = ({ data }: Props) => {
+  const [timeRange, setTimeRange] = useState("30d");
+
+  const filteredData = data.filter((item) => {
+    const date = new Date(item.date);
+    const referenceDate = new Date("2025-01-01");
+
+    let daysToSubtract = 30;
+    if (timeRange === "90d") {
+      daysToSubtract = 90;
+    } else if (timeRange === "7d") {
+      daysToSubtract = 7;
+    }
+
+    const startDate = new Date(referenceDate);
+    startDate.setDate(startDate.getDate() - daysToSubtract);
+    return date >= startDate;
+  });
+
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>Total Visits</CardTitle>
+      <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 md:flex-row">
+        <div className="grid flex-1 gap-1 text-center md:text-left">
+          <CardTitle>Total Visits</CardTitle>
+          <CardDescription></CardDescription>
+        </div>
+        <Select value={timeRange} onValueChange={setTimeRange}>
+          <SelectTrigger
+            className="w-[160px] rounded-lg md:ml-auto"
+            aria-label="Select a value"
+          >
+            <SelectValue placeholder="Last 30 days" />
+          </SelectTrigger>
+          <SelectContent className="rounded-xl">
+            <SelectItem value="7d" className="rounded-lg">
+              Last 7 days
+            </SelectItem>
+            <SelectItem value="30d" className="rounded-lg">
+              Last 30 days
+            </SelectItem>
+            <SelectItem value="90d" className="rounded-lg">
+              Last 3 months
+            </SelectItem>
+          </SelectContent>
+        </Select>
       </CardHeader>
-      <CardContent>
+      <CardContent className="p-2">
         <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-          <LineChart accessibilityLayer data={data}>
+          <LineChart accessibilityLayer data={filteredData}>
             <CartesianGrid vertical={false} />
             <XAxis
               dataKey="date"
