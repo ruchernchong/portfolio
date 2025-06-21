62ae4d1d3294a8025a3f35cbeb7e16610af9e142
Ru Chern Chong <hello@ruchern.dev>
2025-06-22 03:36:33 +0800

    Remove unused files

--- apps/blog/utils/trackEvent.ts
+++
@@ -1,8 +0,0 @@
-export const trackEvent = (
-  eventName: string,
-  eventData?: Record<string, any>,
-) => {
-  if (typeof window !== "undefined" && window.umami) {
-    window.umami.track(eventName, eventData);
-  }
-};
