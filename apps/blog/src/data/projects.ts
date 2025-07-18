69fd24d1eba0d3c6cbe6700d5f65ec795fee77f6
Ru Chern Chong <hello@ruchern.dev>
2025-07-19 02:42:37 +0800

    chore(blog): update projects

--- apps/blog/src/data/projects.ts
+++ apps/blog/src/data/projects.ts
@@ -17,29 +17,14 @@ const projects: Project[] = [
     ],
     links: ["https://shop.singtel.com"],
   },
-  {
-    name: "Singapore Motor Trends",
-    slug: "singapore-motor-trends",
-    description:
-      "Statistics for car trends in Singapore. Data provided by Land Transport Authority (LTA)",
-    skills: ["next.js", "typescript", "react", "tailwind css", "sst", "aws"],
-    links: ["https://sgmotortrends.com", "https://api.sgmotortrends.com"],
-  },
-  {
-    name: "CPF Contribution Calculator",
-    slug: "cpf-contribution-calculator",
-    description:
-      "A calculator to compute CPF contributions after the 2023 income ceiling changes following Ministry of Finance announcement at the Singapore Budget 2023",
-    skills: [
-      "next.js",
-      "typescript",
-      "react",
-      "redux",
-      "shadcn/ui",
-      "tailwind css",
-    ],
-    links: ["https://cpf-contribution-calculator.ruchern.dev"],
-  },
+  // {
+  //   name: "Singapore Motor Trends",
+  //   slug: "singapore-motor-trends",
+  //   description:
+  //     "Statistics for car trends in Singapore. Data provided by Land Transport Authority (LTA)",
+  //   skills: ["next.js", "typescript", "react", "tailwind css", "sst", "aws"],
+  //   links: ["https://sgmotortrends.com", "https://api.sgmotortrends.com"],
+  // },
 ];
 
 export default projects;
