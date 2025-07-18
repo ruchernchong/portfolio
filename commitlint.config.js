3b70f9d13a7833116b5d1a3d8a471cdcf529d3c1
Ru Chern Chong <hello@ruchern.dev>
2025-07-19 05:26:01 +0800

    chore(ci): setup automated release workflow and code quality tools

---
+++ commitlint.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'blog',
+        'deps',
+        'ci',
+        'docs',
+        'release'
+      ]
+    ],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+        'build',
+        'revert'
+      ]
+    ]
+  }
+};
