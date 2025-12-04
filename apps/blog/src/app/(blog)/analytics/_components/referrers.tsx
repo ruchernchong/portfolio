579fa1e150bee33fe4343b62aa8fc88773ccf849
Ru Chern Chong <hello@ruchern.dev>
2025-12-05 03:30:30 +0800

    refactor: migrate tRPC to server-side only

--- apps/blog/src/app/(blog)/analytics/_components/referrers.tsx
+++ apps/blog/src/app/(blog)/analytics/_components/referrers.tsx
@@ -4,32 +4,31 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/shared/card";
-import { Loader } from "@/components/shared/loader";
-import { trpc } from "@/trpc/client";
 
-export const Referrers = () => {
-  const { data = [], isLoading } = trpc.analytics.getReferrers.useQuery();
-  console.log(data);
+interface ReferrersProps {
+  data: {
+    referrer: string | null;
+    count: number;
+    percent: number;
+  }[];
+}
 
+export const Referrers = ({ data }: ReferrersProps) => {
   return (
     <Card>
       <CardHeader>
         <CardTitle>Top Sources</CardTitle>
       </CardHeader>
       <CardContent>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          data.map(({ referrer, count }) => (
-            <div
-              key={referrer}
-              className="flex justify-between border-neutral-600 border-b py-2 last-of-type:border-none"
-            >
-              <span>{referrer || "Direct / None"}</span>
-              <span>x{count}</span>
-            </div>
-          ))
-        )}
+        {data.map(({ referrer, count }) => (
+          <div
+            key={referrer}
+            className="flex justify-between border-neutral-600 border-b py-2 last-of-type:border-none"
+          >
+            <span>{referrer || "Direct / None"}</span>
+            <span>x{count}</span>
+          </div>
+        ))}
       </CardContent>
     </Card>
   );
--- apps/blog/src/app/(blog)/dashboard/page.tsx
+++ apps/blog/src/app/(blog)/dashboard/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { CodeBracketIcon, StarIcon } from "@heroicons/react/24/solid";
 import { SiGithub } from "@icons-pack/react-simple-icons";
 import { ChartColumnIncreasing } from "lucide-react";
@@ -7,55 +5,53 @@ import { MetricCard } from "@/app/(blog)/analytics/_components/metric-card";
 import { Referrers } from "@/app/(blog)/analytics/_components/referrers";
 import VisitsChart from "@/app/(blog)/analytics/_components/visits-chart";
 import { PageTitle } from "@/components/shared/page-title";
-import { trpc } from "@/trpc/client";
+import { serverTrpc } from "@/server";
 
-const Dashboard = () => {
-  const { data: totalVisits, isLoading: totalVisitsLoading } =
-    trpc.analytics.getTotalVisits.useQuery();
-  const { data: followers, isLoading: followersLoading } =
-    trpc.github.getFollowers.useQuery();
-  const { data: stars, isLoading: starsLoading } =
-    trpc.github.getStars.useQuery();
-  const { data: contributions, isLoading: contributionsLoading } =
-    trpc.github.getContributions.useQuery();
+const Dashboard = async () => {
+  const [totalVisits, followers, stars, contributions, visits, referrers] =
+    await Promise.all([
+      serverTrpc.analytics.getTotalVisits(),
+      serverTrpc.github.getFollowers(),
+      serverTrpc.github.getStars(),
+      serverTrpc.github.getContributions(),
+      serverTrpc.analytics.getVisits(),
+      serverTrpc.analytics.getReferrers(),
+    ]);
 
   return (
     <div className="mx-auto flex max-w-4xl flex-col gap-8">
       <PageTitle
         title="Dashboard"
-        description="📊 Just sharing a quick snapshot of a few personal wins and milestones I've hit along the way—it's been a fun ride so far!"
+        description="Just sharing a quick snapshot of a few personal wins and milestones I've hit along the way - it's been a fun ride so far!"
         className="flex flex-col gap-4"
       />
 
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <MetricCard
           title="Total Site Visits"
-          value={totalVisitsLoading ? "..." : (totalVisits ?? 0)}
+          value={totalVisits ?? 0}
           icon={<ChartColumnIncreasing />}
         />
         <MetricCard
           title="GitHub Followers"
-          value={followersLoading ? "..." : (followers ?? 0)}
+          value={followers ?? 0}
           icon={<SiGithub className="size-8" />}
         />
         <MetricCard
           title="GitHub Stars"
-          value={starsLoading ? "..." : (stars ?? 0)}
+          value={stars ?? 0}
           icon={<StarIcon className="size-8" />}
         />
         <MetricCard
           title="Total Commits"
           value={
-            contributionsLoading
-              ? "..."
-              : (contributions?.contributionsCollection
-                  .totalCommitContributions ?? 0)
+            contributions?.contributionsCollection.totalCommitContributions ?? 0
           }
           icon={<CodeBracketIcon className="size-8" />}
         />
       </div>
-      <VisitsChart />
-      <Referrers />
+      <VisitsChart data={visits} />
+      <Referrers data={referrers} />
     </div>
   );
 };
