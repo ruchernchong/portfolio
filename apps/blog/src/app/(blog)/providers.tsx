e555b2aa94989ce2a5d63f72f2b93e604327eacc
Ru Chern Chong <hello@ruchern.dev>
2025-12-18 19:30:02 +0800

    chore: remove Convex integration

--- apps/blog/src/app/(blog)/providers.tsx
+++ apps/blog/src/app/(blog)/providers.tsx
@@ -1,25 +1,20 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
 export const Providers = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <ConvexProvider client={convex}>
-      <HeroUIProvider>
-        <NextThemesProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-          disableTransitionOnChange
-        >
-          {children}
-        </NextThemesProvider>
-      </HeroUIProvider>
-    </ConvexProvider>
+    <HeroUIProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem={false}
+        disableTransitionOnChange
+      >
+        {children}
+      </NextThemesProvider>
+    </HeroUIProvider>
   );
 };
